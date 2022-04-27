1f89e9f17ce1e85ae60713406723a6788aa1aa3c
shravani <shravani.mula@thundersoft.com>
2022-04-27 18:32:46 +0530

    area of circle

---
+++ circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#define PI 3.14
+int main()
+{
+	int rad;
+	float area,cir;
+	printf("enter a radius of circle\n");
+	scanf("%d",&rad);
+	printf("arae of circle:%f\n",area=PI*rad*rad);
+	printf("circumference of circle:%f\n",cir=2*PI*rad);
+}
+
